53628675d78f8f4141686e75a96cd5b2fa6ba41a
Fabricio Diaz <fabriciodiazpino@outlook.com>
2020-12-07 09:30:35 -0500

    real

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text = "Hola " + textBox1.Text;
+        }
     }
 }
